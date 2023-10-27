925e420ddb3a0c4dde7b4a7962570b6b56c87fb5
Miguel Silva <miguel.luciano@alunoescolassociais.org.br>
2023-10-27 12:12:08 -0300

    Corrige Bug

--- controller.js
+++ controller.js
@@ -1,94 +1,39 @@
-'use strict';
+function preencherEndereco() {
+    const cep = document.getElementById("cep").value;
+    const ruaInput = document.getElementById("rua");
+    const bairroInput = document.getElementById("bairro");
+    const cidadeInput = document.getElementById("cidade");
+    const estadoInput = document.getElementById("estado");
 
-// Função para limpar o formulário
-const limparFormulario = () => {
-    document.getElementById('rua').value = '';
-    document.getElementById('bairro').value = '';
-    document.getElementById('cidade').value = '';
-    document.getElementById('estado').value = '';
-    document.getElementById('phone').value = '';
-    document.getElementById('dob').value = '';
-    document.getElementById('rg').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('confirmEmail').value = '';
-    document.getElementById('password').value = '';
-    document.getElementById('confirmPassword').value = '';
-}
-
-// Verifica se o CEP é válido
-const eNumero = (numero) => /^[0-9]+$/.test(numero);
-const cepValido = (cep) => cep.length == 8 && eNumero(cep);
-
-// Função para preencher o formulário
-const preencherFormulario = (endereco) => {
-    document.getElementById('rua').value = endereco.logradouro;
-    document.getElementById('bairro').value = endereco.bairro;
-    document.getElementById('cidade').value = endereco.localidade;
-    document.getElementById('estado').value = endereco.uf;
-}
-
-// Função para validar email
-const validarEmail = () => {
-    const email = document.getElementById('email').value;
-    const confirmEmail = document.getElementById('confirmEmail').value;
-    
-    if (email !== confirmEmail) {
-        console.log("emails não conferem")
-        document.getElementById('email').value='';
-        document.getElementById('email').focus();
-        //document.getElementById('email').value = '';
-        //document.getElementById('confirmEmail').value = '';
-    }else{
-        console.log("Emails ok");
-    }
-}
-
-// Função para validar senha
-const validarSenha = () => {
-    const senha = document.getElementById('password').value;
-    const confirmSenha = document.getElementById('confirmPassword').value;
-    
-    if (senha !== confirmSenha) {
-        console.log("senha  não conferem")
-        document.getElementById('password').value = '';
-        document.getElementById('confirmPassword').focus();
-        
-    }else{
-        console.log("senha ok");
-    
-}
-    }
-
-
-// Consumo da API da ViaCEP
-const pesquisarCep = async () => {
-    limparFormulario();
-    const cep = document.getElementById('cep').value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
-
-    if (cepValido(cep)) {
-        try {
-            const response = await fetch(url);
-            const endereco = await response.json();
-
-            if (endereco.erro) {
-                alert('CEP não encontrado');
-            } else {
-                preencherFormulario(endereco);
-            }
-        } catch (error) {
-            console.error('Erro ao buscar o CEP: ' + error);
-        }
+    if (/^\d{8}$/.test(cep)) {
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            .then(response => response.json())
+            .then(data => {
+                ruaInput.value = data.logradouro;
+                bairroInput.value = data.bairro;
+                cidadeInput.value = data.localidade;
+                estadoInput.value = data.uf;
+            })
+            .catch(error => {
+                console.error("Erro ao buscar o CEP:", error);
+            });
     } else {
-        alert('CEP incorreto');
+        console.error("CEP inválido. Digite um CEP válido.");
     }
 }
 
-// Adiciona evento ao input do CEP para executar a função pesquisarCep
-document.getElementById('cep').addEventListener('focusout', pesquisarCep);
+function cadastrarUsuario() {
+    const camposObrigatorios = ["nome", "sobrenome", "email", "senha", "dataNascimento", "genero", "cpf", "rg","cep","rua","bairro","cidade","estado"];
 
-// Adiciona eventos para validar email e senha
-//document.getElementById('confirmEmail').addEventListener('change', validarEmail);
-document.getElementById('password').addEventListener('change', validarSenha);
-document.getElementById('confirmPassword').addEventListener('change', validarSenha);
+    for (const campo of camposObrigatorios) {
+        const campoInput = document.getElementById(campo);
+        if (!campoInput.value) {
+            alert(`O campo ${campoInput.getAttribute("id")} é obrigatório e não foi preenchido.`);
+            return;
+        }
+    }
 
+    alert("Usuário cadastrado com sucesso!");
+}
+
+document.getElementById("cep").addEventListener("blur", preencherEndereco);
